c55f7b6a283c22cb30470e0bd9bf0a56701e8cd5
RodionfromHSE <rodionkhvorostov@gmail.com>
2023-10-24 19:19:15 +0200

    Project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Translator focused on exact science texts',
+    author='Kovrigin A., Khvorostov R.',
+    license='MIT',
+)
